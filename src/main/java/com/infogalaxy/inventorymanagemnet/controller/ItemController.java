28f977dc76693791de4ccffd4fe02d6dec07a9f4
sejaltamboli <sejaltamboli0602@gmail.com>
2023-08-21 12:34:49 +0530

    Arraylist and Entity layer

--- src/main/java/com/infogalaxy/inventorymanagemnet/controller/ItemController.java
+++ src/main/java/com/infogalaxy/inventorymanagemnet/controller/ItemController.java
@@ -1,24 +1,41 @@
 package com.infogalaxy.inventorymanagemnet.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.infogalaxy.inventorymanagemnet.entity.item;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/item/api")
 public class ItemController {
 
+    ArrayList<item> itemArrayList = new ArrayList<>();
     @GetMapping("home")
     public String homepage() {
         return "Welcome to Item API";
     }
 
     @PostMapping("/additem")
-    public String addItem() {
-        return "Item Added";
+    public item addItem(@RequestBody item item1) {
+        itemArrayList.add(item1);
+        return item1;
+    }
 
+    @GetMapping("/getitem")
+    public item getitem() {
+          item item1 = new item();
+          item1.setId(1);
+          item1.setName("Pen");
+          item1.setDescription("Gel Pen");
+          item1.setPrice(5);
+          item1.setQuantity(100);
+          item1.setStatus("Available");
+          return item1;
+    }
 
+    @GetMapping("/getallitem")
+    public ArrayList<item> getallitems() {
+        return itemArrayList;
     }
 }
---
+++ src/main/java/com/infogalaxy/inventorymanagemnet/entity/item.java
@@ -0,0 +1,59 @@
+package com.infogalaxy.inventorymanagemnet.entity;
+
+public class item {
+
+    private int id;
+    private String name;
+    private String description;
+    private int price;
+    private int quantity;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
